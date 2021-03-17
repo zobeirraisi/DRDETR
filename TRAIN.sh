ead93ee5265cb513b4225852c8c2ffe2a9802555
Zobeir Raisi <zraisi@uwaterloo.ca>
2021-03-16 21:48:15 -0400

    first commit

---
+++ TRAIN.sh
@@ -0,0 +1,1 @@
+python main.py --batch_size 1 
---
+++ inference.sh
@@ -0,0 +1,2 @@
+# python demo.py --resume out/checkpoint.pth --img_path /media/karim/Data/Me/CODE/WORK/Deformable-DETR/data/coco/val2017/0000007.jpg
+python demo.py --resume out/checkpoint.pth --img_path data/coco/val2017/043.jpg
